8175bd6534b402433a252e3842699b2b1891c983
adam-conway <adam.n.conway@gmail.com>
2018-07-16 09:13:26 -0600

    Added favorite foods route

---
+++ routes/api/v1/favorite_foods.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+const FavoriteFood = require('../../../models/favorites.js')
+
+/* GET favorite foods listing. */
+router.get('/', function(req, res, next) {
+  FavoriteFood.all()
+  .then((data) => {
+    res.status(200).json(data.rows)
+  })
+});
+
+module.exports = router;
