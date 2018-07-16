d3955d351d3f64a5790b0deaaf5e0ac1f7c0485e
adam-conway <adam.n.conway@gmail.com>
2018-07-16 11:40:58 -0600

    added sub query for times eaten

--- models/favorite_food.js
+++ models/favorite_food.js
@@ -4,14 +4,18 @@ const database = require('knex')(configuration)
 
 const all = () => {
   return database.raw(
-    `SELECT count(foods.id) AS timesEaten,
-        COALESCE(json_agg(json_build_object('name', foods.name, 'calories', foods.calories, 'mealsWhenEaten', meals.name)) FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods
-        FROM meal_foods
-        LEFT JOIN foods ON meal_foods.food_id = foods.id
-        LEFT JOIN meals ON meal_foods.meal_id = meals.id
-        GROUP BY foods.id
-        HAVING count(foods.id) > 0
-        ORDER BY timesEaten DESC;`
+    `SELECT timesEaten,
+            json_agg(json_build_object('name', name, 'calories', calories)) AS foods
+     FROM
+         (
+           SELECT DISTINCT foods.name, foods.calories, COUNT(foods.id) AS timesEaten
+           FROM foods
+           JOIN meal_foods ON foods.id = meal_foods.food_id
+           GROUP BY foods.id
+           ORDER BY timesEaten DESC
+         ) connectingStuffs
+     GROUP BY timesEaten
+     ORDER BY timesEaten DESC`
   );
 };
 
